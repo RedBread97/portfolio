e003ab51b0733c5309a0c9bc7ba25ed54e390afc
Alex Lane <97alane@gmail.com>
2022-05-24 22:31:30 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container">
+            <h1>Alexander Lane</h1>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
